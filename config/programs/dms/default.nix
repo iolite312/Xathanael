16cde813fff162dde770d9108fb14038a8b9c5b5
iolite312 <68647023+iolite312@users.noreply.github.com>
2026-09-12 23:30:42 +0200

    adding dms to the rice

---
+++ config/programs/dms/default.nix
@@ -0,0 +1,15 @@
+{
+  programs.dms-shell = {
+    enable = true;
+
+    systemd = {
+      enable = true;
+      restartIfChanged = true;
+    };
+
+    enableSystemMonitoring = true;
+    enableVPN = true;
+    enableDynamicTheming = true;
+    enableCalenderEvents = true;
+  };
+}
